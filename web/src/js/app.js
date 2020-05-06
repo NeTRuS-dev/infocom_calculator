e64498df2c2544200f501199482595563f6f4a60
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-06 17:35:24 +0300

    fixed a mistake

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,7 +1,11 @@
 import '@/scss/styles.scss';
 import Vue from 'vue';
+import BaseCalculatorComponent from "@/js/components/baseCalculatorComponent";
+
 new Vue({
-    el:'#app_entry',
-    template:`<baseCalculatorComponent>
-            </baseCalculatorComponent>`,
+    el: '#app_entry',
+    template: `
+        <base-calculator-component>
+        </base-calculator-component>`,
+    components: {BaseCalculatorComponent},
 });
