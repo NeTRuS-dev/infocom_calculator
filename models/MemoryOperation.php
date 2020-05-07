6ee8fba34d5d8e453e8489ec45f99fd21d8c4f13
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-07 10:31:58 +0300

    Operations hierarchy

---
+++ models/MemoryOperation.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\models;
+
+
+class MemoryOperation extends Operation
+{
+
+}
---
+++ models/Operation.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\models;
+
+
+abstract class Operation extends \yii\base\Model
+{
+
+}
